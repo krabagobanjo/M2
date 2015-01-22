098778288d53f3d5a30946bebf2fae99f68ec0ab
Slugpuppy <jake@slugpuppy.com>
2015-01-21 20:21:23 -0500

    Eddited AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,20 +71,20 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  Person3 p3 = new Person3("P3");
 		  toPrint += p3.toString("gburdell3");
                   
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
-                  
-                  //Person5 replace P5 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
-                  
-                  //Person6 replace P6 with your name
-                  //and gburdell6 with your gt id
-                  Person6 p6 = new Person6("P6");
-                  toPrint += p6.toString("gburdell6");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("P4");
+          toPrint += p4.toString("gburdell4");
+          
+          //Person5 replace P5 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("JakeWilliams");
+          toPrint += p5.toString("jwilliams392");
+          
+          //Person6 replace P6 with your name
+          //and gburdell6 with your gt id
+          Person6 p6 = new Person6("P6");
+          toPrint += p6.toString("gburdell6");
 		  
         System.out.println(toPrint);
         
